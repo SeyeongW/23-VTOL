7bef1663548b7fc03768c090e059331baf6190de
Seyeong Wang <seyeong186@gmail.com>
2025-12-02 05:05:02 +0900

    change file format py to c

---
+++ camera/siyi_gimbal.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <math.h>
+
+// 짐벌 설정
+#define SERVER_IP "192.168.144.25"
+#define SERVER_PORT 37260
+#define DEADZONE 0.05
+#define KP_X 0.5
+#define KP_Y 0.5
+
+// CRC16 테이블 (CCITT)
+const uint16_t crc16_tab[256]= {
+    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50A5, 0x60C6, 0x70E7,
+    0x8108, 0x9129, 0xA14A, 0xB16B, 0xC18C, 0xD1AD, 0xE1CE, 0xF1EF,
+    0x1231, 0x0210, 0x3273, 0x2252, 0x52B5, 0x4294, 0x72F7, 0x62D6,
+    0x9339, 0x8318, 0xB37B, 0xA35A, 0xD3BD, 0xC39C, 0xF3FF, 0xE3DE,
+    0x2462, 0x3443, 0x0420, 0x1401, 0x64E6, 0x74C7, 0x44A4, 0x5485,
+    0xA56A, 0xB54B, 0x8528, 0x9509, 0xE5EE, 0xF5CF, 0xC5AC, 0xD58D,
+    0x3653, 0x2672, 0x1611, 0x0630, 0x76D7, 0x66F6, 0x5695, 0x46B4,
+    0xB75B, 0xA77A, 0x9719, 0x8738, 0xF7DF, 0xE7FE, 0xD79D, 0xC7BC,
+    0x48C4, 0x58E5, 0x6886, 0x78A7, 0x0840, 0x1861, 0x2802, 0x3823,
+    0xC9CC, 0xD9ED, 0xE98E, 0xF9AF, 0x8948, 0x9969, 0xA90A, 0xB92B,
+    0x5AF5, 0x4AD4, 0x7AB7, 0x6A96, 0x1A71, 0x0A50, 0x3A33, 0x2A12,
+    0xDBFD, 0xCBDC, 0xFBBF, 0xEB9E, 0x9B79, 0x8B58, 0xBB3B, 0xAB1A,
+    0x6CA6, 0x7C87, 0x4CE4, 0x5CC5, 0x2C22, 0x3C03, 0x0C60, 0x1C41,
+    0xEDAE, 0xFD8F, 0xCDEC, 0xDDCD, 0xAD2A, 0xBD0B, 0x8D68, 0x9D49,
+    0x7E97, 0x6EB6, 0x5ED5, 0x4EF4, 0x3E13, 0x2E32, 0x1E51, 0x0E70,
+    0xFF9F, 0xEFBE, 0xDFDD, 0xCFFC, 0xBF1B, 0xAF3A, 0x9F59, 0x8F78,
+    0x9188, 0x81A9, 0xB1CA, 0xA1EB, 0xD10C, 0xC12D, 0xF14E, 0xE16F,
+    0x1080, 0x00A1, 0x30C2, 0x20E3, 0x5004, 0x4025, 0x7046, 0x6067,
+    0x83B9, 0x9398, 0xA3FB, 0xB3DA, 0xC33D, 0xD31C, 0xE37F, 0xF35E,
+    0x02B1, 0x1290, 0x22F3, 0x32D2, 0x4235, 0x5214, 0x6277, 0x7256,
+    0xB5EA, 0xA5CB, 0x95A8, 0x8589, 0xF56E, 0xE54F, 0xD52C, 0xC50D,
+    0x34E2, 0x24C3, 0x14A0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405,
+    0xA7DB, 0xB7FA, 0x8799, 0x97B8, 0xE75F, 0xF77E, 0xC71D, 0xD73C,
+    0x26D3, 0x36F2, 0x0691, 0x16B0, 0x6657, 0x7676, 0x4615, 0x5634,
+    0xD94C, 0xC96D, 0xF90E, 0xE92F, 0x99C8, 0x89E9, 0xB98A, 0xA9AB,
+    0x5844, 0x4865, 0x7806, 0x6827, 0x18C0, 0x08E1, 0x3882, 0x28A3,
+    0xCB7D, 0xDB5C, 0xEB3F, 0xFB1E, 0x8BF9, 0x9BD8, 0xABBB, 0xBB9A, 0x4A75, 0x5A54, 0x6A37, 0x7A16, 0x0AF1, 0x1AD0, 0x2AB3, 0x3A92,
+    0xFD2E, 0xED0F, 0xDD6C, 0xCD4D, 0xBDAA, 0xAD8B, 0x9DE8, 0x8DC9, 0x7C26, 0x6C07, 0x5C64, 0x4C45, 0x3CA2, 0x2C83, 0x1CE0, 0x0CC1,
+    0xEF1F, 0xFF3E, 0xCF5D, 0xDF7C, 0xAF9B, 0xBFBA, 0x8FD9, 0x9FF8, 0x6E17, 0x7E36, 0x4E55, 0x5E74, 0x2E93, 0x3EB2, 0x0ED1, 0x1EF0
+};
+
+// CRC16 계산 함수
+uint16_t calculate_crc16(unsigned char *ptr, int len) {
+    uint16_t crc = 0;
+    while (len-- > 0) {
+        crc = (crc << 8) ^ crc16_tab[((crc >> 8) ^ *ptr++) & 0xff];
+    }
+    return crc;
+}
+
+// 짐벌 속도 명령 전송
+void send_gimbal_speed(int sockfd, struct sockaddr_in *addr, int yaw_speed, int pitch_speed) {
+    // 속도 범위 제한 (-100 ~ 100)
+    if (yaw_speed > 100) yaw_speed = 100;
+    if (yaw_speed < -100) yaw_speed = -100;
+    if (pitch_speed > 100) pitch_speed = 100;
+    if (pitch_speed < -100) pitch_speed = -100;
+
+    unsigned char packet[12]; // 12 bytes total size
+
+    // Header
+    packet[0] = 0x55; // STX
+    packet[1] = 0x66; // STX
+    packet[2] = 0x01; // CTRL (Need ACK)
+    
+    // Data Length (2 bytes, Little Endian) -> 0x02 0x00
+    packet[3] = 0x02; 
+    packet[4] = 0x00; 
+    
+    // Sequence (2 bytes, Little Endian) -> 0x00 0x00 (Fixed for simplicity)
+    packet[5] = 0x00;
+    packet[6] = 0x00;
+    
+    // Command ID -> 0x07 (GIMBAL_SPEED)
+    packet[7] = 0x07;
+    
+    // Data (Payload) -> Yaw(int8) + Pitch(int8)
+    packet[8] = (char)yaw_speed;
+    packet[9] = (char)pitch_speed;
+    
+    // CRC16 Calculation (from index 0 to 9)
+    uint16_t crc = calculate_crc16(packet, 10);
+    
+    // CRC (2 bytes, Little Endian)
+    packet[10] = (unsigned char)(crc & 0xFF); // Low byte
+    packet[11] = (unsigned char)((crc >> 8) & 0xFF); // High byte
+
+    // Send UDP packet
+    sendto(sockfd, packet, 12, 0, (struct sockaddr *)addr, sizeof(*addr));
+    
+    printf("[TX] Yaw: %d, Pitch: %d | Hex: ", yaw_speed, pitch_speed);
+    for(int i=0; i<12; i++) printf("%02X ", packet[i]);
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    // 인자로 4개의 좌표값(ymin, xmin, ymax, xmax)을 받습니다.
+    if (argc != 5) {
+        printf("Usage: %s <ymin> <xmin> <ymax> <xmax>\n", argv[0]);
+        // 인자 없으면 정지 명령 보내고 종료
+        // (실제 사용 시에는 소켓 열고 0,0 보내는 로직 필요)
+        return -1;
+    }
+
+    // 좌표 파싱 (0.0 ~ 1.0)
+    float ymin = atof(argv[1]);
+    float xmin = atof(argv[2]);
+    float ymax = atof(argv[3]);
+    float xmax = atof(argv[4]);
+
+    // 중심점 계산
+    float cx = (xmin + xmax) / 2.0f;
+    float cy = (ymin + ymax) / 2.0f;
+
+    // 에러 계산 (중앙 0.5 기준)
+    float err_x = cx - 0.5f;
+    float err_y = cy - 0.5f;
+
+    // Deadzone 체크
+    if (fabs(err_x) < DEADZONE) err_x = 0;
+    if (fabs(err_y) < DEADZONE) err_y = 0;
+
+    // 속도 명령 계산 (P 제어)
+    int yaw_cmd = (int)(err_x * 100 * KP_X);
+    int pitch_cmd = (int)(-err_y * 100 * KP_Y); // Pitch 방향 반전 필요시 부호 변경
+
+    // 소켓 초기화
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        return -1;
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERVER_PORT);
+    servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    // 명령 전송
+    send_gimbal_speed(sockfd, &servaddr, yaw_cmd, pitch_cmd);
+
+    close(sockfd);
+    return 0;
+}
